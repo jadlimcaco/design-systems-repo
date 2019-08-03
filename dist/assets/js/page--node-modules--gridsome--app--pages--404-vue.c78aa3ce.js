a0f96c54f773b8a5b15cad95be8f6bcdeae6a274
Jad Limcaco <jadlimcaco@gmail.com>
2019-08-03 13:58:35 -0700

    Update

--- dist/assets/js/page--node-modules--gridsome--app--pages--404-vue.c78aa3ce.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{248:function(n,t,e){"use strict";e.r(t);var s=e(91),u=Object(s.a)({},function(){var n=this.$createElement;return(this._self._c||n)("h1",[this._v("404 - not found")])},[],!1,null,null,null);t.default=u.exports}}]);
--- src/main.js
+++ src/main.js
@@ -11,5 +11,4 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
-
 }
